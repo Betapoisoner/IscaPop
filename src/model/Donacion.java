e701acf55cd796c0eadce7321874f3d84dd03513
Betapoisoner <sdhds954@gmail.com>
2024-04-19 08:49:49 +0200

    Reparaci0on donacion

--- src/model/Donacion.java
+++ src/model/Donacion.java
@@ -3,23 +3,17 @@ package model;
 public class Donacion {
 
     private String id_Donacion;
-    private String centroDonante;
-    private String centroReceptor;
+    private String centro_Donante;
+    private String centro_Receptor;
+    private String fecha_don;
 
-    public Donacion(String idDonacion, String centroDonante, String centroReceptor) {
-        this.id_Donacion = idDonacion;
-        this.centroDonante = centroDonante;
-        this.centroReceptor = centroReceptor;
+	public Donacion(String id_Donacion, String centro_Donante, String centro_Receptor, String fecha_don) {
+        this.id_Donacion = id_Donacion;
+        this.centro_Donante = centro_Donante;
+        this.centro_Receptor = centro_Receptor;
+        this.fecha_don = fecha_don;
     }
 
-    public String getIdDonacion() {
-        return id_Donacion;
-    }
 
-    public void setIdDonacion(String idDonacion) {
-        this.id_Donacion = idDonacion;
-    }
-
-   
 
 }
